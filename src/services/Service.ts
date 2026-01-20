149359215d5e55c40d4e2a7a727f1bc49b29aab6
Pedro <operdop@proton.me>
2026-01-20 11:56:07 -0300

    feat tema, deletar e cadastrar

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-wrapper-object-types */
+/* eslint-disable @typescript-eslint/no-unsafe-function-type */
 import axios from "axios";
 
 const api = axios.create({
@@ -14,4 +16,23 @@ export const cadastrarUsuario = async (url: string, dados: object, setDados: Fun
 export const login = async (url: string, dados: object, setDados: Function) => {
     const resposta = await api.post(url, dados)
     setDados(resposta.data)
+}
+
+export const buscar = async (url: string, setDados: Function, header: Object) => {
+    const resposta = await api.get(url, header)
+    setDados(resposta.data)
+}
+
+export const cadastrar = async (url: string, dados: Object, setDados: Function, header: Object) => {
+    const resposta = await api.post(url, dados, header)
+    setDados(resposta.data)
+}
+
+export const atualizar = async (url: string, dados: Object, setDados: Function, header: Object) => {
+    const resposta = await api.put(url, dados, header)
+    setDados(resposta.data)
+}
+
+export const deletar = async (url: string, header: Object) => {
+    await api.delete(url, header)
 }
