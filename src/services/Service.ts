d9796902a143906f335fe907503811df27dda033
Pedro <operdop@proton.me>
2026-01-24 12:43:25 -0300

    deploy

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -3,7 +3,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'https://blogpessoal-ym1n.onrender.com'
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
